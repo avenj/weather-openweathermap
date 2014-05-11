39d142194807e1b0bcfa93959bd6988fa3ec9f15
Jon Portnoy <avenj@cobaltirc.org>
2014-05-11 05:39:40 -0400

    outline for hourly (3hr) forecasts
    	new file:   lib/Weather/OpenWeatherMap/Request/Forecast/Hourly.pm
    	new file:   lib/Weather/OpenWeatherMap/Result/Forecast/Hourly.pm
    	new file:   t/request/forecast/hourly.t
    	new file:   t/result/forecast/hourly.t

---
+++ lib/Weather/OpenWeatherMap/Result/Forecast/Hourly.pm
@@ -0,0 +1,8 @@
+# FIXME these produce a *lot* of results, may want to generate via lazy iter
+# FIXME needs cache update (new prefix/type handler)
+# FIXME maybe a reorg?
+#  {Request,Result}::Forecast
+#    Forecast::Daily
+#      Forecast::Daily::Day
+#    Forecast::Hourly
+#      Forecast::Hourly::Block
