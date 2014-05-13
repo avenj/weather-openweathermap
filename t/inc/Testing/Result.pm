d8b26f72a238e27cc4df7e9a23433ab39893c629
Jon Portnoy <avenj@cobaltirc.org>
2014-05-12 20:46:34 -0400

    Testing::Result::Current reasonably complete
    	modified:   t/inc/Testing/Result.pm
    	modified:   t/inc/Testing/Result/Current.pm

--- t/inc/Testing/Result.pm
+++ t/inc/Testing/Result.pm
@@ -16,12 +16,32 @@ sub get_mock_json {
 
 test 'data hash has keys' => sub {
   my ($self) = @_;
-  ok !$self->result_obj->data->is_empty;
+  ok !$self->result_obj->data->is_empty
 };
 
 test 'error is false' => sub {
   my ($self) = @_;
-  ok !$self->result_obj->error;
+  ok !$self->result_obj->error
+};
+
+test 'is_success is true' => sub {
+  my ($self) = @_;
+  ok $self->result_obj->is_success
+};
+
+test 'json looks correct' => sub {
+  my ($self) = @_;
+  ok $self->result_obj->json eq $self->mock_json
+};
+
+test 'response_code indicates success' => sub {
+  my ($self) = @_;
+  cmp_ok $self->result_obj->response_code, '==', 200
+};
+
+test 'request objects match' => sub {
+  my ($self) = @_;
+  ok $self->result_obj->request == $self->request_obj
 };
 
 
