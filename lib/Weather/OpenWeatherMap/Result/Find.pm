0d7098cd9b9959fc18d511b5cba9bd07fe1886ba
Jon Portnoy <avenj@cobaltirc.org>
2014-08-15 13:18:48 -0400

    Test for memory cycles
    Some POD tweaks
    
    	modified:   dist.ini
    	modified:   lib/Weather/OpenWeatherMap/Result/Find.pm
    	modified:   t/inc/Testing/Result.pm

--- lib/Weather/OpenWeatherMap/Result/Find.pm
+++ lib/Weather/OpenWeatherMap/Result/Find.pm
@@ -104,8 +104,9 @@ L<Weather::OpenWeatherMap::Result::Current> instance:
 
 See L<Weather::OpenWeatherMap::Result::Current>.
 
-The current weather returned by a Find is not quite as complete as a full
-L<Weather::OpenWeatherMap::Request::Current> might return. In particular:
+The current weather returned by a Find is not quite as complete as that
+returned by an actual L<Weather::OpenWeatherMap::Request::Current>. In
+particular:
 
 =over
 
